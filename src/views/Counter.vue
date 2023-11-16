a3eadbfe60250830681b37042595e728a60c8673
masvidal778 <nuria.masvidal@gmail.com>
2023-11-16 18:06:10 +0100

    creating counter and increase_decrease buttons

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,13 +1,31 @@
 <script>
-import {defineComponent} from 'vue'
+import {defineComponent, ref} from 'vue'
 
 export default defineComponent({
-name: "Counter"
+  name: "Counter",
+  //props: {} anirien aquí
+  //emits: [] aniria aquí
+  setup() {
+
+    //ref embolcalla la variable en un objecte reactiu, par això el value
+    const counter = ref(5)
+
+    return {
+      counter,
+      increase: () => counter.value++,
+      decrease: () => counter.value--
+    }
+
+  }
 })
 </script>
 
 <template>
-  $END$
+<h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
 </template>
 
 <style scoped>
